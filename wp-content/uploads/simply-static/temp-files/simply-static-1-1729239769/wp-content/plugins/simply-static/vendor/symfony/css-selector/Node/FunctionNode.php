1328b668b7a64c27447315476255129953db55ca
liujiawen1118 <liujiawen1118@gmail.com>
2024-10-18 19:39:14 +0800

    10/18 19:39

---
+++ wp-content/uploads/simply-static/temp-files/simply-static-1-1729239769/wp-content/plugins/simply-static/vendor/symfony/css-selector/Node/FunctionNode.php
@@ -0,0 +1,24 @@
+<br>
+<font size="1"><table class="xdebug-error xe-uncaught-exception" dir="ltr" border="1" cellspacing="0" cellpadding="1">
+<tr><th align="left" bgcolor="#f57900" colspan="5">
+<span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Fatal error: Uncaught Error: Class "Symfony\Component\CssSelector\Node\AbstractNode" not found in D:\wamp64\www\TestWordPress\wp-content\plugins\simply-static\vendor\symfony\css-selector\Node\FunctionNode.php on line <i>26</i>
+</th></tr>
+<tr><th align="left" bgcolor="#f57900" colspan="5">
+<span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Error: Class "Symfony\Component\CssSelector\Node\AbstractNode" not found in D:\wamp64\www\TestWordPress\wp-content\plugins\simply-static\vendor\symfony\css-selector\Node\FunctionNode.php on line <i>26</i>
+</th></tr>
+<tr><th align="left" bgcolor="#e9b96e" colspan="5">Call Stack</th></tr>
+<tr>
+<th align="center" bgcolor="#eeeeec">#</th>
+<th align="left" bgcolor="#eeeeec">Time</th>
+<th align="left" bgcolor="#eeeeec">Memory</th>
+<th align="left" bgcolor="#eeeeec">Function</th>
+<th align="left" bgcolor="#eeeeec">Location</th>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">1</td>
+<td bgcolor="#eeeeec" align="center">0.0010</td>
+<td bgcolor="#eeeeec" align="right">528048</td>
+<td bgcolor="#eeeeec">{main}(  )</td>
+<td title="D:\wamp64\www\TestWordPress\wp-content\plugins\simply-static\vendor\symfony\css-selector\Node\FunctionNode.php" bgcolor="#eeeeec">...\FunctionNode.php<b>:</b>0</td>
+</tr>
+</table></font>
